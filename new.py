6297223bfdc4537d280d8d543bdfdfdb7a8eed99
sreekanth <asreekanth1313@gmail.com>
2022-10-11 13:28:01 +0530

    hhgcg

---
+++ new.py
@@ -0,0 +1,10 @@
+employee ={
+    'name': 'Tim',
+    'age': 30,
+    'birthday': "1990-03-10",
+    'job': 'DevOps Engineer'
+}
+employee['job'] = 'Software Engineer'
+employee.__delitem__('age')
+for key, value in employee.items():
+    print(key, ' : ', value)
