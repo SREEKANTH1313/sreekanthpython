1f014da5a1eb84e0d1645240003a3d16aee41980
ashokthuraka <110800498+ashokthuraka@users.noreply.github.com>
2022-10-13 14:53:21 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -2,7 +2,7 @@ my_list = [1, 2, 2, 4, 4, 5, 6, 8, 10, 13, 22, 35, 52, 83]
 new_list = []
 number = []
 for i in my_list:
-    if i >= 10:
+    if i >=10:
         new_list.append(i)
 print(new_list)
 user_input = int(input("enter the number : "))
