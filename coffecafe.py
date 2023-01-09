902f128f7b6e6d0dcb06ec1b9731ed7986226326
SREEKANTH A <110668483+SREEKANTH1313@users.noreply.github.com>
2023-01-09 11:07:51 +0530

    Add files via upload

--- coffecafe.py
+++ coffecafe.py
@@ -13,19 +13,30 @@ while True:
     quantity = int(input("Enter how many cups you want: "))
     if user == 'tea' or user == '1':
         tea1 = tea1 + quantity * 15
-        # print('Your Tea Bill is : ', tea1)
     elif user == 'ginger' or user == '2':
         Ginger1 = Ginger1 + quantity * 20
-        # print('Your Ginger Bill is : ', int(Ginger1))
     elif user == 'boost' or user == '3':
         Boost1 = Boost1 + quantity * 25
-        # print('Your  Boost Bill is : ', Boost1)
     elif user == 'coffee' or user == '4':
         coffee1 = coffee1 + quantity * 20
-        # print('Your Coffee Bill is : ', coffee1)
     user2 = input("Please enter yes/ no if you want another  items: ").lower()
     if user2.lower() == 'no':
         break
 total = tea1 + Ginger1 + coffee1 + Boost1
 print('your total bill is : ', total)
+
+# QR  Code
+# import pyqrcode
+# import qrcode
+# # # from pyzbar import decode
+# # # from PIL
+# # import image
+# q = qrcode.QRCode(version=1, box_size=10, error_correction=qrcode.constants.ERROR_CORRECT_L, border=5)
+# q.add_data('b')
+# a = pyqrcode.create('Welcome to  My  Coffee Cafe')
+# a.png('Mycode.png', scale=8)
+# # b = qrcode.make('https://forms.gle/7px5fiJWqeJzfGno8')
+# b = qrcode.make('https://forms.gle/GcnDL5sGKD1MaT1h6')
+# b.save('Mycode.png')
+#
 
