dd03f37a3b3a47ff17fb9ababf6eeb6d0402f16a
SREEKANTH A <110668483+SREEKANTH1313@users.noreply.github.com>
2023-01-09 11:29:48 +0530

    Even And Odd

--- even.py
+++ even.py
@@ -1,20 +1,22 @@
-def sree_12(even):
-    print("The even numbers are: ")
-    for i in even:
+def sree_12():
+    
+    even = []
+    odd = []
+
+    for i in mylist1:
         if i % 2 == 0:
-            print(i)
+            even.append(i)
+        else:
+            odd.append(i)
+    print("The Even Number List is: ", even)
+    print("The Odd Number List is: ", odd)
 
 
 mylist1 = [12, 13, 17, 22, 45, 68, 34, 24, 57, 98]
 mylist1.sort()
-sree_12(mylist1)
+sree_12()
 
 
 # output
-# The even numbers are:
-# 12
-# 22
-# 24
-# 34
-# 68
-# 98
+# The Even Number List is:  [12, 22, 24, 34, 68, 98]
+# The Odd Number List is:  [13, 17, 45, 57]
