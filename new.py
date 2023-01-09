95e2d961465eb390866dd4effa573ad1c462e485
SREEKANTH A <110668483+SREEKANTH1313@users.noreply.github.com>
2023-01-09 11:18:23 +0530

    employee details

--- new.py
+++ new.py
@@ -8,3 +8,8 @@ employee['job'] = 'Software Engineer'
 employee.__delitem__('age')
 for key, value in employee.items():
     print(key, ' : ', value)
+
+#     ---OutPut---
+#     name  :  Tim
+#     birthday  :  1990-03-10
+#     job  :  Software Engineer
