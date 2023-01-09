04dac11d587273caae4e60ec151b08d479bebf99
SREEKANTH A <110668483+SREEKANTH1313@users.noreply.github.com>
2023-01-09 12:19:10 +0530

    Add files via upload

---
+++ swapcase.py
@@ -0,0 +1,13 @@
+# swap case without using swap_case() method.
+a = input("Enter the any string: ")
+b = ''
+for i in a:
+    if i.islower():
+        b = b + i.upper()
+    elif i.isupper():
+        b = b + i.lower()
+    elif i.isascii():
+        b = b + i
+print(b)
+# Enter the any string: 'sreekanthAlaKuntLA123"Python2."'
+# 'SREEKANTHaLAkUNTla123"pYTHON2."'
